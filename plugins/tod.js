031e1016b789dfbb362b54a00e76b8615b2a5e4b
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-13 18:38:06 +0700

    p

--- plugins/tod.js
+++ plugins/tod.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command }) => {
   if (/^tod$/i.test(command)) {
-    conn.send3Button(m.chat, 'Truth or Dare', footer, 'Truth', '#truth', 'Dare', '#dare', 'RANDOM', `${pickRandom(['#dare', '#truth'])}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+    conn.send3Button(m.chat, 'Truth or Dare', footer, 'Truth', '#truth', 'Dare', '#dare', m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
   }
 }
 handler.help = ['tod']
@@ -9,7 +9,3 @@ handler.tags = ['fun']
 handler.command = /^(tod)$/i
 
 module.exports = handler
-
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
