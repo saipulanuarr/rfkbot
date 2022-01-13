691e8006d4818f859b9648aaf140e5de9b49a9d5
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-13 15:59:42 +0700

    p

--- plugins/yvebot.js
+++ plugins/yvebot.js
@@ -3,8 +3,8 @@ let PhoneNumber = require('awesome-phonenumber')
 let levelling = require('../lib/levelling')
   
 let handler = async (m, { conn }) => {
-let caption = `*HALO Bang*\n*Saya Mikebot*\n*Pilih Menu Di Bawah Yaa Bree*\n*Spam = Banned*\n*Telp = Blok*\n\n*Join Group WhatsApp*\nhttps://chat.whatsapp.com/E9g9yrUEFVk6QBZ14arnIh\n\n*Untuk Melihat Menu Bot Ketik .menu Atau .MikeBot*`.trim()
-conn.send3Button(m.chat, caption, `Powered By Baileys\nCreated Mikebot By Dawnfrost\n`.trim(), '🧾MENU', '.?', '🍭Owner MikeBot', '.owner', '📖Info Bot', '.infobot', m)
+let caption = `*HALO Bang*\n*Saya Yuki Bot*\n*Pilih Menu Di Bawah Yaa Bree*\n*Spam = Banned*\n*Telp = Blok*\n\n*Join Group WhatsApp*\nhttps://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL\n\n*Untuk Melihat Menu Bot Ketik .menu Atau .YukiBot*`.trim()
+conn.send3Button(m.chat, caption, `Powered By Baileys\nCreated Yuki Bot By King Of Bear\n`.trim(), '🧾MENU', '.?', '🍭Owner Yuki Bot', '.owner', '📖Info Bot', '.infobot', m)
 }
 
 handler.command = /^(hel)$/i
