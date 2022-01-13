0610771e58bbde3a0b1e7895679bbfd9d9b98dd7
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-13 18:20:51 +0700

    p

--- plugins/truth.js
+++ plugins/truth.js
@@ -1,12 +1,12 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, command }) => {
-  if (/^tod$/i.test(command)) {
-    conn.send3Button(m.chat, 'Truth or Dare', footer, 'Truth', '#truth', 'Dare', '#dare', 'RANDOM', `${pickRandom(['#dare', '#truth'])}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
-  }
-  if (/^truth$/i.test(command)) {
-  let handler  = async (m, { conn }) => {
+let handler  = async (m, { conn }) => {
   conn.reply(m.chat,`“${pickRandom(global.truth)}”`, m)
 }
+handler.help = ['truth', 'dare']
+handler.tags = ['quotes']
+handler.command = /^(truth)$/i
+
+module.exports = handler
+
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
@@ -114,24 +114,3 @@ global.truth = [
     "Apa pendapatmu tentang perselingkuhan?",
     "Apa rahasia yang tidak pernah kamu ceritakan kepada siapapun?",
     "Apa waasa saat paling memalukan dalam hidup Anda?"]
-  
-  }
-  if (/^dare$/i.test(command)) {
-    let res = await fetch('https://raw.githubusercontent.com/saipulanuar/Truth-Or-Dare/main/dare.js')
-    if (!res.ok) throw eror
-    let json = await res.json()
-    if (json.message == "") throw json
-    conn.send2Button(m.chat, json.message, footer, 'Truth', '#truth', 'Dare', '#dare', m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
-
-
-  }
-}
-handler.help = ['tod']
-handler.tags = ['fun']
-handler.command = /^(tod|truth|dare)$/i
-
-module.exports = handler
-
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
