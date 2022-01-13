1c5cdd2cd8bf1f55f678515ce90316f7537e9866
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-13 14:29:42 +0700

    p

--- plugins/How.js
+++ plugins/How.js
@@ -9,9 +9,9 @@ ${command} *${text}*
     }
   } : {})
 }
-handler.help = ['gay', 'pintar', 'cantik', 'ganteng', 'gabut', 'gila', 'lesbi', 'stress', 'bucin', 'jones', 'sadboy'].map(v => 'how' + v + ' siapa?')
+handler.help = ['gay', 'pintar', 'cantik', 'ganteng', 'gabut', 'gila', 'lesbi', 'stress', 'bucin', 'jones', 'sadboy', 'beban'].map(v => 'how' + v + ' siapa?')
 handler.tags = ['kerang']
-handler.command = /^how(gay|pintar|cantik|ganteng|gabut|gila|lesbi|stress?|bucin|jones|sadboy)/i
+handler.command = /^how(gay|pintar|cantik|ganteng|gabut|gila|lesbi|stress?|bucin|jones|sadboy|beban)/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
