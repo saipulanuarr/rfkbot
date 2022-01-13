1e1c7a715694a6d43d34f5b3578c6ccb164159b9
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-13 14:32:47 +0700

    p

--- plugins/TQTO.js
+++ plugins/TQTO.js
@@ -1,19 +1,19 @@
 //ngapain cok? mau ganti nama yak?
 //hargai lah yg buat,gw cape² ngebuat ini tapi kalian nya malah enak²an ubah nama doang
-//kalo mau ada nama kalian chat dulu ke ibnu,izin dulu biar sama² enak
+//kalo mau ada nama kalian chat dulu ke saya,izin dulu biar sama² enak
 //di tambahin nama boleh tapi di ilagin jangan walau satu nama pun
-//yg gk tau nomer raku nih nomer nya
-//http://wa.me/6287834993722?text=bg%20izin%20taroh%20nama%20di%20bot%20mu%20yak
+//yg gk tau nomer aku nih nomer nya
+//http://wa.me/6288279268363?text=bg%20izin%20taroh%20nama%20di%20bot%20mu%20yak
 let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `
 *Pertanyaan:* ${m.text}
 *Jawaban:* ${pickRandom(['Ya','Mungkin iya','Mungkin','Mungkin tidak','Tidak','Tidak mungkin'])}
 `.trim(), m)
 }
-handler.help = ['ɴᴜʀᴜᴛᴏᴍᴏ','ʀᴇɴᴅʏᴄʀᴀғᴛ','ɪʙɴᴜ ɴʀ','ʙᴀᴋᴀ ʙᴏᴛᴢ','ʙᴀᴋᴀ ᴛᴇᴀᴍ']
+handler.help = ['ɴᴜʀᴜᴛᴏᴍᴏ','ʀᴇɴᴅʏᴄʀᴀғᴛ','ɪʙɴᴜ ɴʀ','ʙᴀᴋᴀ ʙᴏᴛᴢ','ʙᴀᴋᴀ ᴛᴇᴀᴍ','ᴋɪɴɢ ᴏғ ʙᴇᴀʀ']
 handler.tags = ['thnks']
 handler.customPrefix = /(\?$)/
-handler.command = /^rakuganteng$/i
+handler.command = /^kingofbear$/i
 handler.owner = false
 handler.mods = false
 handler.premium = true
