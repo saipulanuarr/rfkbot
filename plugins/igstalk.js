bdf14284a568482b187843accd254f494694e6eb
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-14 21:59:50 +0700

    p

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,9 +1,9 @@
 const fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!text) throw `contoh:\n${usedPrefix + command} drak_ipul123`
+  if (!args[0]) throw `contoh:\n${usedPrefix + command} drak_ipul123`
 
-  let res = await fetch('https://viko-api.herokuapp.com/api/stalk/ig?apikey=vinko&query=${text}')
-  if (!res.ok) throw eror
+  let res = await fetch('https://viko-api.herokuapp.com/api/stalk/ig?apikey=vinko&query=${args[0]}')
+  if (!result.ok) throw eror
   let json = await res.json()
   if (json.status != 200) throw json
   conn.sendMessage(m.chat, `Nama: ${json.result.Name}\n*Bio:* ${json.result.Biodata}\n*Followers:* ${json.result.Jumlah_Followers}\n*Following:* ${json.result.Jumlah_Following}\n*Posts:* ${json.result.Jumlah_Post}`, m, 0)
