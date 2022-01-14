b17e80e7287330fbcc4f5b3f24f76a078c57944a
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-14 13:01:15 +0700

    p

--- plugins/owneryve.js
+++ plugins/owneryve.js
@@ -2,7 +2,7 @@ let handler = function (m) {
 	this.sendContact(m.chat, '6288279268363', 'Owner YukiBot :)', m)
 }
 
-handler.customPrefix = ['🍭Owner Mike'] 
+handler.customPrefix = ['🍭Owner YukiBot'] 
 handler.command = new RegExp
 
 module.exports = handler
