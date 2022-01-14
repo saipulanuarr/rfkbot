a9ab4c2ef180c70a664eb6b496737874995320c4
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-14 21:36:09 +0700

    p

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -2,11 +2,11 @@ const fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) throw `contoh:\n${usedPrefix + command} drak_ipul123`
 
-  let res = await fetch(global.API('zekais', '/igs', { username: args[0] }))
+  let res = await fetch('https://viko-api.herokuapp.com/api/stalk/ig?apikey=vinko&query=${args[0]}')
   if (!res.ok) throw eror
   let json = await res.json()
   if (json.status != 200) throw json
-  conn.sendFile(m.chat, json.data.profilehd, 'eror.jpg', `*Nama:* ${json.data.fullname}\n*Bio:* ${json.data.bio}\n*Followers:* ${json.data.follower}\n*Following:* ${json.data.following}\n*Posts:* ${json.data.timeline}\n*Private:* ${json.data.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/stikerinbot`, m, 0, { thumbnail: await (await fetch(json.data.profilehd)).buffer() })
+  conn.sendFile(m.chat, json.result.Profile_pic, 'eror.jpg', `*Nama:* ${json.result.Name}\n*Bio:* ${json.result.Biodata}\n*Followers:* ${json.result.Jumlah_Followers}\n*Following:* ${json.result.Jumlah_Following}\n*Posts:* ${json.result.Jumlah_Post}\n*Private:* ${json.result.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/drak_ipul123`, m, 0, { thumbnail: await (await fetch(json.result.Profile_pic)).buffer() })
 }
 handler.help = ['igstalk <username>']
 handler.tags = ['tools']
