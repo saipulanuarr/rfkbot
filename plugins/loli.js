bf8568d7b257c3b49eb4fb1ba0d9b483d0e0c20d
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-15 00:05:58 +0700

    p

--- plugins/loli.js
+++ plugins/loli.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
      let handler  = async (m, { conn, usedPrefix, command }) => {
 m.reply(wait)
-heum = await fetch(`https://api.lolhuman.xyz/api/random/loli?apikey=${lolkey}`)
+heum = await fetch(`https://hardianto.xyz/api/anime/loli?apikey=hardianto`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'Next', `${usedPrefix + command}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
