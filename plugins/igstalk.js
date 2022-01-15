88c11b0bf428395a84c182cc17a3701a28996251
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-15 15:08:26 +0700

    P

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,37 +1,15 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...username nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/igstalk', {
-    nama: args[0]
-  }, 'APIKEY'))
+const fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  if (!args[0]) throw `contoh:\n${usedPrefix + command} drak_ipul123`
+
+  let res = await fetch(`https://api.xteam.xyz/dl/igstalk?nama=${args[0]}&APIKEY=KingOfBear`)
+  if (!res.ok) throw eror
   let json = await res.json()
-  if (res.status != 200) throw json
+  if (json.status != 200) throw json
   if (json.result.error) throw json.result.message
-  let {
-    Name,
-    Username,
-    is_verified,
-    media_count,
-    Jumlah_Followers,
-    Jumlah_Following,
-    Biodata,
-    external_url,
-    Profile_pic,
-    hd_profile_pic_url_info,
-    is_private
-  } = json.result
-  let pp = Profile_pic.url || Profile_pic
-  let caption = `
-  *Nama:* ${json.result.Name}
-  *Bio:* ${json.result.Biodata}
-  *Followers:* ${json.result.Jumlah_Followers}
-  *Following:* ${json.result.Jumlah_Following}
-  *Posts:* ${json.result.Jumlah_Post}
-`.trim()
-   conn.sendFile(m.chat, json.result.Profile_pic, 'eror.jpg', m, 0, { thumbnail: await (await fetch(json.result.Profile_pic)).buffer() })
-  else m.reply(caption)
+  conn.sendFile(m.chat, json.result.Profile_pic, 'eror.jpg', `*Nama:* ${json.result.Name}\n*Bio:* ${json.result.Biodata}\n*Followers:* ${json.result.Jumlah_Followers}\n*Following:* ${json.result.Jumlah_Following}\n*Posts:* ${json.result.Jumlah_Post}\n*Private:* ${json.result.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/drak_ipul123`, m, 0, { thumbnail: await (await fetch(json.result.Profile_pic)).buffer() })
 }
-handler.help = ['igstalk <username>']
+handler.help = ['igstalk <username> (Dalam Perbaikan)']
 handler.tags = ['tools']
 handler.command = /^(igstalk)$/i
 handler.limit = true
