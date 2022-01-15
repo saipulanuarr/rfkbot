dd5e13c2fc6237ec5c95f0440374adffe3b76924
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-15 14:02:47 +0700

    p

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,12 +1,37 @@
-const fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `contoh:\n${usedPrefix + command} stikerinbot`
-
-  let res = await fetch(`https://apikey-bear3.herokuapp.com/api/stalk/ig?apikey=vinko&query=${args[0]}`)
-  if (!res.ok) throw eror
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...username nya mana?'
+  let res = await fetch(global.API('xteam', '/dl/igstalk', {
+    nama: args[0]
+  }, 'APIKEY'))
   let json = await res.json()
-  if (json.status) throw json
-  conn.sendFile(m.chat, json.result.Profile_pic, 'eror.jpg', `*Nama:* ${json.result.Name}\n*Bio:* ${json.result.Biodata}\n*Followers:* ${json.result.Jumlah_Followers}\n*Following:* ${json.result.Jumlah_Following}\n*Posts:* ${json.result.Jumlah_Post}\n*Private:* ${json.result.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/drak_ipul123`, m, 0, { thumbnail: await (await fetch(json.result.Profile_pic)).buffer() })
+  if (res.status != 200) throw json
+  if (json.result.error) throw json.result.message
+  let {
+    full_name,
+    username,
+    is_verified,
+    media_count,
+    follower_count,
+    following_count,
+    biography,
+    external_url,
+    profile_pic_url,
+    hd_profile_pic_url_info,
+    is_private
+  } = json.result.user
+  let pp = hd_profile_pic_url_info.url || profile_pic_url
+  let caption = `
+${full_name} *(@${username})* ${is_verified ? '✓' : ''}
+https://instagram.com/${username}
+${is_private ? 'Post Hidden by User' : ('*' + media_count + '* Post(s)')}
+Following *${following_count}* User(s)
+*${follower_count}* Followers
+*Bio:*
+${biography}${external_url ? '\n*External URL:* ' + external_url : ''}
+`.trim()
+  if (pp) conn.sendFile(m.chat, pp, 'ppig.jpg', caption, m)
+  else m.reply(caption)
 }
 handler.help = ['igstalk <username>']
 handler.tags = ['tools']
