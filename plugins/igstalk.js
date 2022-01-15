b662046b525a1b1e276fa3065b5d29fa24fce5d9
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-15 13:32:02 +0700

    p

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,11 +1,11 @@
-const fetch = require('node-fetch')
+let fetch = require('node-fetch')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `contoh:\n${usedPrefix + command} stikerinbot`
 
   let res = await fetch(`https://viko-api.herokuapp.com/api/stalk/ig?apikey=vinko&query=${args[0]}`)
   if (!res.ok) throw eror
   let json = await res.json()
-  if (json.status != 200) throw json
+  if (json.status) throw json
   conn.sendMessage(m.chat, `Nama: ${json.result.Name}\n*Bio:* ${json.result.Biodata}\n*Followers:* ${json.result.Jumlah_Followers}\n*Following:* ${json.result.Jumlah_Following}\n*Posts:* ${json.result.Jumlah_Post}`, m, 0)
 }
 handler.help = ['igstalk <username>']
