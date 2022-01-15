2a9a24761d681f8c94b3f191a047012e684635ae
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-15 15:43:32 +0700

    p

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -1,19 +1,39 @@
-const fetch = require('node-fetch')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `contoh:\n${usedPrefix + command} drak_ipul123`
-
-  let res = await fetch(`https://api.xteam.xyz/dl/igstalk?nama=${args[0]}&APIKEY=KingOfBear`)
-  if (!res.ok) throw eror
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...username nya mana?'
+  let res = await fetch(`https://hardianto.xyz/api/download/instadl?url=${args[0]}?utm_source=ig_web_copy_link&apikey=hardianto`)
   let json = await res.json()
-  if (json.status != 200) throw json
+  if (res.status != 200) throw json
   if (json.result.error) throw json.result.message
+  let {
+    fullName,
+    username,
+    is_verified,
+    medias,
+    followers,
+    following,
+    biography,
+    downloadUrl,
+    profilePicUrl,
+    hd_profilePicUrl_info,
+    is_private
+  } = json.result.user
+  let pp = hd_profilePicUrl_info.url || profilePicUrl
   let caption = `
-  *Nama:* ${json.result.Name}\n*Bio:* ${json.result.Biodata}\n*Followers:* ${json.result.Jumlah_Followers}\n*Following:* ${json.result.Jumlah_Following}\n*Posts:* ${json.result.Jumlah_Post}\n*Private:* ${json.result.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/drak_ipul123
-  `.trim()
-   else m.reply(caption)
+${fullName} *(@${username})* ${is_verified ? '✓' : ''}
+https://instagram.com/${username}
+${is_private ? 'Post Hidden by User' : ('*' + medias + '* Post(s)')}
+Following *${following}* User(s)
+*${followers}* Followers
+*Bio:*
+${biography}${external_url ? '\n*External URL:* ' + downloadUrl : ''}
+`.trim()
+  if (pp) conn.sendFile(m.chat, pp, 'ppig.jpg', caption, m)
+  else m.reply(caption)
 }
-handler.help = ['igstalk <username> (Dalam Perbaikan)']
-handler.tags = ['tools']
+handler.help = ['igstalk'].map(v => v + ' <username>')
+handler.tags = ['downloader']
+
 handler.command = /^(igstalk)$/i
-handler.limit = true
+
 module.exports = handler
