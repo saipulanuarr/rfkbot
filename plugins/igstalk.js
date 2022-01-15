913675daa5007136d5f01030f750fd260c312a75
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-15 14:44:04 +0700

    p

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -8,7 +8,13 @@ let handler = async (m, { conn, args }) => {
   if (res.status != 200) throw json
   if (json.result.error) throw json.result.message
   let caption = `
-conn.sendFile(m.chat, json.result.Profile_pic, 'eror.jpg', `*Nama:* ${json.result.Name}\n*Bio:* ${json.result.Biodata}\n*Followers:* ${json.result.Jumlah_Followers}\n*Following:* ${json.result.Jumlah_Following}\n*Posts:* ${json.result.Jumlah_Post}\n*Private:* ${json.result.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/drak_ipul123`, m, 0, { thumbnail: await (await fetch(json.result.Profile_pic)).buffer() })
+`*Nama:* ${json.result.Name}
+  *Bio:* ${json.result.Biodata}
+  *Followers:* ${json.result.Jumlah_Followers}
+  *Following:* ${json.result.Jumlah_Following}
+  *Posts:* ${json.result.Jumlah_Post}
+  *Private:* ${json.result.private ? 'Ya' : 'Tidak'}
+  http://instagram.com/${json.result.Username}, m, 0, { thumbnail: await (await fetch(json.result.Profile_pic)).buffer() })
 `.trim()
   else m.reply(caption)
 }
