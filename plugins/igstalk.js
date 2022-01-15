2a525c0f086a0b9078a94a7c042fe0d7beb903ca
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-15 14:10:25 +0700

    p

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -8,27 +8,27 @@ let handler = async (m, { conn, args }) => {
   if (res.status != 200) throw json
   if (json.result.error) throw json.result.message
   let {
-    full_name,
-    username,
+    Name,
+    Username,
     is_verified,
     media_count,
-    follower_count,
-    following_count,
-    biography,
+    Jumlah_Followers,
+    Jumlah_Following,
+    Biodata,
     external_url,
-    profile_pic_url,
+    Profile_pic,
     hd_profile_pic_url_info,
     is_private
   } = json.result.user
   let pp = hd_profile_pic_url_info.url || profile_pic_url
   let caption = `
-${full_name} *(@${username})* ${is_verified ? '✓' : ''}
+${Name} *(@${Username})* ${is_verified ? '✓' : ''}
 https://instagram.com/${username}
 ${is_private ? 'Post Hidden by User' : ('*' + media_count + '* Post(s)')}
-Following *${following_count}* User(s)
-*${follower_count}* Followers
+Following *${Jumlah_Following}* User(s)
+*${Jumlah_Followers}* Followers
 *Bio:*
-${biography}${external_url ? '\n*External URL:* ' + external_url : ''}
+${Biodata}${external_url ? '\n*External URL:* ' + external_url : ''}
 `.trim()
   if (pp) conn.sendFile(m.chat, pp, 'ppig.jpg', caption, m)
   else m.reply(caption)
