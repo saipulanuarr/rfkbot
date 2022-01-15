0f9a955b0345db72bd6ee9de71d2c62a81127a9a
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-15 13:38:55 +0700

    P

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -6,7 +6,7 @@ if (!args[0]) throw `contoh:\n${usedPrefix + command} stikerinbot`
   if (!res.ok) throw eror
   let json = await res.json()
   if (json.status) throw json
-  conn.sendMessage(m.chat, `Nama: ${json.result.Name}\n*Bio:* ${json.result.Biodata}\n*Followers:* ${json.result.Jumlah_Followers}\n*Following:* ${json.result.Jumlah_Following}\n*Posts:* ${json.result.Jumlah_Post}`, m, 0)
+  conn.sendFile(m.chat, json.result.Profile_pic, 'eror.jpg', `*Nama:* ${json.result.Name}\n*Bio:* ${json.result.Biodata}\n*Followers:* ${json.result.Jumlah_Followers}\n*Following:* ${json.result.Jumlah_Following}\n*Posts:* ${json.result.Jumlah_Post}\n*Private:* ${json.result.private ? 'Ya' : 'Tidak'}\n\nhttp://instagram.com/drak_ipul123`, m, 0, { thumbnail: await (await fetch(json.result.Profile_pic)).buffer() })
 }
 handler.help = ['igstalk <username>']
 handler.tags = ['tools']
