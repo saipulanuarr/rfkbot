731f4ea5e769d01ef7016724c630c330e1d99e9a
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-16 20:46:31 +0700

    p

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, usedPrefix, command }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
 m.reply(wait)
-heum = await fetch(`https://api.lolhuman.xyz/api/random/nsfw/waifu?apikey=${lolkey}`)
+heum = await fetch(`https://hardianto.xyz/api/anime/random?nsfw=hentai&apikey=hardianto`)
    if (!heum) throw json
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'NEXT', `${usedPrefix + command}`, m)
