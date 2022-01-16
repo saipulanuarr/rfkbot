9da24c5da4be2c9f3ea673ef97c97e04e9feb902
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-16 21:42:49 +0700

    p

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw 'Uhm..url nya mana?'
 m.reply(wait)
-let res = await fetch(`https://apikey-bear2.herokuapp.com/api/tiktod/?url=${args[0]}&apikey=${bearkey}`)
+let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=${lolkey}&url=${args[0]}`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
