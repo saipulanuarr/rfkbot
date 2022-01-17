da938af208a82588ba4187463c4d0e4c1c45d6d1
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-17 18:27:17 +0700

    p

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(`https://i.ibb.co/rkTg7B0/donasi.jpg`)).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ DANA : [0882-7926-8363]
-├ SAWERIA : [https://saweria.co/raraharsita2]
-├ SELAIN DANA VIA QR AJA ATAU SAWERIA
+┌〔 𝘿𝙤𝙣𝙖𝙨𝙞 • 𝙀𝙢𝙤𝙣𝙚𝙮 〕
+├ _DANA_ : *0882-7926-8363*
+├ _SAWERIA_ : *https://saweria.co/raraharsita2*
+├ *SELAIN DANA VIA QR AJA ATAU SAWERIA*
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
