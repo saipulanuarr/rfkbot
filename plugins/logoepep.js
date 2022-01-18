6ba54a03463aa910411d30f95df83a3025a24103
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-18 13:50:04 +0700

    p

--- plugins/logoepep.js
+++ plugins/logoepep.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
      let handler  = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw `Uhm...Teksnya mana?\nContoh: ${usedPrefix + command} Bear`
 m.reply(wait)
-heum = await fetch(https://api.zeks.me/api/epep?apikey=HCea8n9SQlhEQsbVuBPTIEW3c8i&text=${text}`)
+heum = await fetch(`https://api.zeks.me/api/epep?apikey=HCea8n9SQlhEQsbVuBPTIEW3c8i&text=${text}`)
     json = await heum.buffer()
    conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'Next', `${usedPrefix + command}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 
