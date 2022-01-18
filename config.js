1d718c94cee88032eaacaabd4a48cf1f93358730
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-18 12:32:55 +0700

    p

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ global.APIKeys = { // APIKey Here
   'https://api.xteam.xyz': 'KingOfBear',
   'https://api.lolhuman.xyz': 'rey2k21',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
-  'https://api.zeks.me': 'apivinz',
+  'https://api.zeks.me': 'HCea8n9SQlhEQsbVuBPTIEW3c8i',
   'https://pencarikode.xyz': 'pais',
   'https://apikey-bear.herokuapp.com': 'KingOfBear',
   'https://apikey-bear2.herokuapp.com': 'KingOfBear',
@@ -48,7 +48,7 @@ global.APIKeys = { // APIKey Here
 }
 
 lolkey = 'rey2k21'
-zekskey = 'apivinz'
+zekskey = 'HCea8n9SQlhEQsbVuBPTIEW3c8i'
 xteamkey = 'KingOfBear'
 bearkey = 'KingOfBear'
 vinkey = 'vinko'
