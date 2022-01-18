b4909cd6328f58e0faade9b7334ae60e57321b4d
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-18 13:35:51 +0700

    p

--- plugins/logoepep.js
+++ plugins/logoepep.js
@@ -2,7 +2,7 @@ let fetch = require("node-fetch")
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `Uhm...Teksnya mana?\nContoh: ${usedPrefix + command} Bear`
   m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
-  let res = await fetch(`https://api.zeks.me/api/epep?apikey=HCea8n9SQlhEQsbVuBPTIEW3c8i&text=${text}`)
+  let res = await fetch(`https://api.zeks.me/api/epep?apikey=${zekskey}&text=${text}`)
   if (!res.ok) throw eror
   let json = await res.json()
   if (!json.result) throw 'Err!'
