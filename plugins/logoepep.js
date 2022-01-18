151a3050b31046951d07a1e997e0f4e98339eb16
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-18 14:57:58 +0700

    p

--- plugins/logoepep.js
+++ plugins/logoepep.js
@@ -1,19 +1,12 @@
-
 let util = require('util')
 let path = require('path')
 let { spawn } = require('child_process')
 
 // HartA tahta Xteam
 let handler  = async (m, { conn, text, usedPrefix, command }) => {
-  let d = new Date
-  let tgl = d.toLocaleDateString('id-Id')
-  let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
-conn.logoepep = conn.logoepep ? conn.logoepep : {}
- if (m.chat in conn.logoepep) throw 'Masih ada yang sedang membuat\nLogo Epep\ndi chat ini... tunggu sampai selesai'
  if (!text) throw `Uhm...Teksnya mana?\nContoh: ${usedPrefix + command} Bear`
- else conn.logoepep[m.chat] = true
  m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
- conn.sendFile(m.chat, global.API('xteam', '/api/epep', 'APIKEY', { text, }), 'Logo Epep.png', 'Nih udah jadi Logo epepmu...\n *_Tetap Support:_* *King Of Bear*', m, 0, { thumbnail: await (await fetch(global.API)).buffer() })
+await conn.sendFile(m.chat, global.API('xteam', '/api/epep', 'APIKEY', { text, }), 'Harta Tahta.png', 'Nih udah jadi Harta tahtamu...\n *_Tetap Support:_* *King Of Bear*', m)
 }
 handler.help = ['logoepep'].map(v => v + ' <teks>')
 handler.tags = ['tools']
