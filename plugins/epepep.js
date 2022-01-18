201ae5a9c8561c1de0e4f1754bb57c62408ab455
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-18 20:09:42 +0700

    p

--- plugins/epepep.js
+++ plugins/epepep.js
@@ -1,15 +1,24 @@
-let fetch = require("node-fetch")
-let handler = async (m, { conn }) => {
-  m.reply(wait)
-  let res = await fetch(`https://apikey-bear3.herokuapp.com/api/maker/epep?apikey=${bearkey}&text=KingOfBear`)
-  if (!res.ok) throw eror
-  let json = await res.json()
-  if (!json.result) throw 'Err!'
-  let thumbnail = await (await fetch(json.result)).buffer()
-  conn.sendFile(m.chat, json.result, '8bit.png', json.caption, m, 0, { thumbnail })
+
+let fetch = require('node-fetch')
+     let handler  = async (m, { conn, usedPrefix, command }) => {
+m.reply(wait)
+heum = await fetch(`https://apikey-bear3.herokuapp.com/api/maker/epep?apikey=KingOfBear&text=KingOfBear`)
+    json = await heum.buffer()
+   conn.sendButtonImg(m.chat, json, kasihcaption, footer, 'Next', `${usedPrefix + command}`, m, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+
 }
 handler.help = ['8bit']
-handler.tags = ['internet']
-handler.command = /^(8bit)$/i
+handler.tags = ['anime']
+handler.command = /^8bit$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
 
 module.exports = handler
