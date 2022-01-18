b0a7eb3f0dc636f2ddd1a8c42c43766194b2d7da
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-18 13:32:34 +0700

    p

--- plugins/logoepep.js
+++ plugins/logoepep.js
@@ -1,25 +1,14 @@
-let util = require('util')
-let path = require('path')
-let { spawn } = require('child_process')
-
-// HartA tahta Xteam
-let handler  = async (m, { conn, text, usedPrefix, command }) => {
-  let d = new Date
-  let tgl = d.toLocaleDateString('id-Id')
-  let hari = d.toLocaleDateString('id-Id', { weekday: 'long' })
-conn.logoepep = conn.logoepep ? conn.logoepep : {}
- if (m.chat in conn.logoepep) throw 'Masih ada yang sedang membuat\nLogo Epep\ndi chat ini... tunggu sampai selesai'
- if (!text) throw `Uhm...Teksnya mana?\nContoh: ${usedPrefix + command} Bear`
- else conn.logoepep[m.chat] = true
- m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
- let url = await fetch(`https://api.zeks.me/api/epep?apikey=HCea8n9SQlhEQsbVuBPTIEW3c8i&text=${text}`)
-if (!res.ok) throw eror
+let fetch = require("node-fetch")
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `Uhm...Teksnya mana?\nContoh: ${usedPrefix + command} Bear`
+  m.reply('_Sedang membuat..._\n*Mohon tunggu sekitar 1 menit*')
+  let res = await fetch(`https://api.zeks.me/api/epep?apikey=HCea8n9SQlhEQsbVuBPTIEW3c8i&text=${text}`)
+  if (!res.ok) throw eror
   let json = await res.json()
   if (!json.result) throw 'Err!'
   let thumbnail = await (await fetch(json.result)).buffer()
-  conn.sendFile(m.chat, json.result, 'Logo Epep.png', json.caption, m, 0, { thumbnail })
+  conn.sendFile(m.chat, json.result, 'darkjoke.png', json.caption, m, 0, { thumbnail })
 }
-
 handler.help = ['logoepep'].map(v => v + ' <teks>')
 handler.tags = ['tools']
 handler.command = /^(logoepep)$/i
