e09ec44d9878db0daafbb0b541061abd103b5956
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-18 10:47:58 +0700

    p

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -1,13 +1,19 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 	conn.send3ButtonLoc(m.chat, await (await fetch(fla + 'sewa bot')).buffer(), `╠═〘 Beli Bot 〙 ═
-╠➥ *1 Bulan* :      *Rp 20.000,00*
-╠➥ *Permanen* : *Rp 30.000,00*
-╠➥ *Premium* :   *Rp 15.000,00*
-╠➥ *Sc Bot* :        *https://www.youtube.com/watch?v=d7dcw2CO4ew*
+╠➥ *1 Bulan* :      *Rp 15.000,00*
+╠➥ *Permanen* : *Rp 25.000,00*
+╠➥ *Premium* :   *Rp 10.000,00*
+╠➥ *Permanen+SC* : *Rp 100.000,00*
+╠➥ *Jasa Run* : *Rp 15.000,00*
+╠➥ *Edit Sc/Nambah Fitur Bot*
+╠➥ *1 Fitur* : *Rp 5.000.00 No Apikey*
+╠➥ *1 Fitur* : *Rp 8.000.00 + Apikey*
+╠➥ *Beli Apikey* : *Rp 20.000.00 = 1 Bulan*
+╠➥ *Sc Bot* :        *https://youtu.be/jJrZYhM8fGQ*
 ║
 ╠═〘 PEMBAYARAN 〙 ═
-╠➥ Gopay, Dana, Dan Pulsa
+╠➥ Gopay, Dana, Dan Ovo
 ║
 ╠═ Tertarik Untuk Beli Bot Ini?
 ╠➥Ketuk Tombol Di Bawah Ya
