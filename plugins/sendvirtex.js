071e016fb903c128fd00da4e786e8018e1058c5a
KingOfBear-YT1 <97571796+KingOfBear-YT1@users.noreply.github.com>
2022-01-19 11:05:15 +0700

    p

---
+++ plugins/sendvirtex.js
@@ -0,0 +1,38 @@
+const { MessageType } = require('@adiwajshing/baileys')
+
+let handler = async(m, { conn, text }) => {
+let [number, pesan] = text.split `|`
+
+    if (!number) return conn.reply(m.chat, 'Silahkan masukan nomor yang akan dikirim virtex', m)
+    let info = `
+    *SAYANG*
+    `.trim()
+    if (text.startsWith('+')) return conn.reply(m.chat, '[!] Tolong masukan Nomor dengan awalaan 62', m)
+    if (text.startsWith('@')) return conn.reply(m.chat, '[!] Tolong masukan Nomor dengan awalaan 62', m)
+
+    let korban = `${number}`
+    var nomor = m.sender
+    let virtex = `*「 DAPAT PESAN LANGSUNG 」*\n\nDari : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${info}\n\n~KING OF BEAR`
+
+    conn.sendMessage(korban + '@s.whatsapp.net', virtex, MessageType.text)
+
+    let logs = `[!] Berhasil mengirim virtex ke nomor ${korban}`
+    conn.reply(m.chat, logs, m)
+}
+handler.help = ['sendvirtex <nomor>', 'sendvirus <nomor>']
+handler.tags = ['virus']
+handler.command = /^(sendvirtex|sendvirus)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+handler.disable = false // klo pake wa mod ubah aja ke false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = false
+
+module.exports = handler
