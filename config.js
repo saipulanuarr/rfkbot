1ca946c51c88504d8342a564f097777a1bc9fc53
KingOfBear-Official <97654014+KingOfBear-Official@users.noreply.github.com>
2022-01-24 21:12:21 +0700

    p

--- config.js
+++ config.js
@@ -16,10 +16,10 @@ gc1 = ''
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whatsapp.com/HAZ6yFgCafUAeDbNH33IrL', ''] // ganti jadi group lu
-global.owner = ['6288279268363'] // Put your number here //owner eval
-global.kontak = ['6288279268363','0'] //Ketika ada yang ngetik #owner
-global.mods = ['0'] // Want some help?
-global.prems = ['6288279268363'] // Premium user has unlimited limit
+global.owner = ['6283128703968'] // Put your number here //owner eval
+global.kontak = ['6283128703968','0'] //Ketika ada yang ngetik #owner
+global.mods = ['6288279268363'] // Want some help?
+global.prems = ['6283128703968'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com', 
@@ -72,7 +72,7 @@ global.author = 'King Of Bear' // ganti aja
 bc = 'King Of Bear' //King Of Bear Broadcast
 footer = '\n©King Of Bear'
 namabot = 'YUKI BOT'
-namalu = 'King Of Bear'
+namalu = 'Riko Store31'
 
 
 // 
